4479acffb3f527ae29f67032cb20a9f4f43cf7cb
frarosset <37275581+frarosset@users.noreply.github.com>
2024-10-14 11:21:48 +0200

    Update commonDomUtilities.js

--- src/js-utilities/commonDomUtilities.js
+++ src/js-utilities/commonDomUtilities.js
@@ -49,3 +49,58 @@ export function resetContent(contentDiv) {
   contentDiv.setAttribute("class", "");
   window.scrollTo(0, 0);
 }
+
+// from: https://stackoverflow.com/a/143889
+// Determines if the passed element is overflowing its bounds,
+// either vertically or horizontally.
+// Will temporarily modify the "overflow" style to detect this
+// if necessary.
+function checkOverflow(el)
+{
+   let curOverflow = el.style.overflow;
+
+   if ( !curOverflow || curOverflow === "visible" )
+      el.style.overflow = "hidden";
+
+   let isOverflowing = el.clientWidth < el.scrollWidth 
+                    || el.clientHeight < el.scrollHeight;
+
+   el.style.overflow = curOverflow;
+
+   return isOverflowing;
+}
+
+// throttle function to avoid calling the actual callback continuously (eg, on resize or scroll)
+// from: https://stackoverflow.com/questions/68751736/throttle-window-scroll-event-in-react-with-settimeout
+function throttle (callbackFn, limit=100) {
+    let wait = false;                  
+    return function () {              
+        if (!wait) {                  
+            callbackFn.call();           
+            wait = true;               
+            setTimeout(function () {
+                callbackFn.call();
+                wait = false;          
+            }, limit);
+        }
+    }
+}
+
+function fitFontSize(elem, defaultFontSize='',delta=0.9){
+    // Initialize the fontSize, if the initial value is provided
+    if (defaultFontSize)
+        elem.style.fontSize = defaultFontSize;
+    let fontSize = getComputedStyle(elem).getPropertyValue('font-size');
+
+    let fontSizeVal,fontSizeUnit; 
+    [fontSizeVal,fontSizeUnit] = splitCSSUnits(fontSize);
+
+    while (checkOverflow(elem)){
+        fontSizeVal *= delta;
+        elem.style.fontSize = fontSizeVal + fontSizeUnit;
+    }
+}
+
+function splitCSSUnits(CSSAttrVal){
+    return [CSSAttrVal.match(/[\d.]+/)[0],CSSAttrVal.match(/[^\d.]+/)[0]];
+}
